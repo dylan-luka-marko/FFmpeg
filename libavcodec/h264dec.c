7f59df7a1d54c6cc9411e58ab2312c94ace32761
Hendrik Leppkes <h.leppkes@gmail.com>
2013-12-29 21:03:02 +0100

    HACK Revert "h264: free the tables and uninitialize the context on flush"
    This reverts commit 9eda9d3322a6ea91830a754f2edee222adae7e67.
    This commit causes the flush inside the DXVA2 init to crash, so we cannot do this here.
    
    The fix isn't required for our use-case anyway, since LAV always does a hard-flush when seeking H.264.

--- libavcodec/h264dec.c
+++ libavcodec/h264dec.c
@@ -502,9 +502,6 @@ static void h264_decode_flush(AVCodecContext *avctx)
     ff_h264_unref_picture(h, &h->cur_pic);
 
     h->mb_y = 0;
-
-    ff_h264_free_tables(h);
-    h->context_initialized = 0;
 }
 
 static int get_last_needed_nal(H264Context *h)
