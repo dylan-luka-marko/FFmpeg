1aaa5bea0e9c91aa9aced7a5e7ba7c814e1ac58a
Hendrik Leppkes <h.leppkes@gmail.com>
2015-10-28 14:30:30 +0100

    asfdec: don't subtract the pre-roll from index entries
    Empiric analysis of a variety of samples has concluded that the index
    entries are written without the preroll.

--- libavformat/asfdec_f.c
+++ libavformat/asfdec_f.c
@@ -1621,7 +1621,7 @@ static int asf_build_simple_index(AVFormatContext *s, int stream_index)
             int pktnum        = avio_rl32(s->pb);
             int pktct         = avio_rl16(s->pb);
             int64_t pos       = s->internal->data_offset + s->packet_size * (int64_t)pktnum;
-            int64_t index_pts = FFMAX(av_rescale(itime, i, 10000) - asf->hdr.preroll, 0);
+            int64_t index_pts = FFMAX(av_rescale(itime, i, 10000), 0);
 
             if (avio_feof(s->pb)) {
                 ret = AVERROR_INVALIDDATA;
